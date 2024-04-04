245cb792a9d2a2c90cd90a350934f6a7f10988bf
leejunyong <163401658+1a2a3a3a@users.noreply.github.com>
2024-04-04 20:39:05 +0900

    Add files via upload

---
+++ arrange.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+cd files
+mv [aA]* ../a/
+mv [bB]* ../b/
+mv [cC]* ../c/
+mv [dD]* ../d/
+mv [eE]* ../e/
+mv [fF]* ../f/
+mv [gG]* ../g/
+mv [hH]* ../h/
+mv [iI]* ../i/
+mv [jJ]* ../j/
+mv [kK]* ../k/
+mv [lL]* ../l/
+mv [mM]* ../m/
+mv [nN]* ../n/
+mv [oO]* ../o/
+mv [pP]* ../p/
+mv [qQ]* ../q/
+mv [rR]* ../r/
+mv [sS]* ../s/
+mv [tT]* ../t/
+mv [uU]* ../u/
+mv [vV]* ../v/
+mv [wW]* ../w/
+mv [xX]* ../x/
+mv [yY]* ../y/
+mv [zZ]* ../z/
